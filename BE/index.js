9ba7d3de6dc82a3273bf427ab642372d28b28c86
Afif Syam Fauzi <afiffauzi2016@gmail.com>
2025-10-15 14:27:57 +0700

    berhasil kirim api

--- BE/index.js
+++ BE/index.js
@@ -12,6 +12,17 @@ const pool = new Pool({
     database: process.env.DB_NAME,
 });
 
+const checkDatabaseConnection = async () => {
+    try {
+        await pool.connect();
+        console.log('✅ Koneksi ke database PostgreSQL berhasil!');
+    } catch (err) {
+        console.error('❌ KONEKSI DATABASE GAGAL:', err.message);
+        // Hentikan aplikasi jika tidak bisa konek ke DB
+        process.exit(1); 
+    }
+};
+
 //middleware
 
 app.use(cors());
@@ -34,16 +45,12 @@ app.get('/api/tasks', async (req, res) => {
     }
 })
 
-app.listen(port, () => {
-    console.log(`Server berjalan di http://localhost:${port}`);
-}); 
-
-
 //enpoint jika ada tigas baru
 
 app.post('/api/tasks', async (req, res) => {
-    const client = await pool.connect();
+   let client;
     try {
+        client = await pool.connect();
         const { title, description, assignee_name, start_date, due_date } = req.body;
         const user_id = 1; 
         await client.query('BEGIN');
@@ -51,12 +58,11 @@ app.post('/api/tasks', async (req, res) => {
         
         const taskValues = [title, description, assignee_name, start_date, due_date, user_id];
         const newTaskResult = await client.query(newTaskQuery, taskValues);
-
         const newTaskId = newTaskResult.rows[0].id;
 
         //masukan data log ke tabel logs
         const logAction = `tugas "${title}" telah dibuat dan ditugaskan kepada ${assignee_name}`;
-        const newLogQuery = 'INSERT INTO logs (task_id, action) VALUES ($1, $2)';
+        const newLogQuery = 'INSERT INTO task_logs (task_id, action) VALUES ($1, $2)';
         await client.query(newLogQuery, [newTaskId, logAction]);
 
         await client.query('COMMIT');
@@ -64,14 +70,23 @@ app.post('/api/tasks', async (req, res) => {
         res.status(201).json({ message: 'Tugas berhasil dibuat', task: {id: newTaskId, title, status: 'Belum Dimulai' }
         });
     }catch (err) {
-        await client.query('ROLLBACK');
-        console.error(err.message);
+        if (client){
+             await client.query('ROLLBACK');
+        }
+        console.error('ERROR:', err.message);
         res.status(500).send('Server Error');
     }finally{
-        client.release();
+        if (client){
+            client.release();
+        }   
     }
 });
 
+ const startServer = async () => {
+    await checkDatabaseConnection(); // Jalankan pengecekan koneksi dulu
     app.listen(port, () => {
-        console.log(`Server berjalan di http://localhost:${port}`);
-});
+        console.log(`🚀 Server berjalan di http://localhost:${port}`);
+    });
+};
+
+startServer();
